cb45172cce1c57d8b7fa0d2d245d7458a34331bb
Simon Barnes <simon@simonbarnes.me>
2023-03-07 07:29:43 +0000

    hide string prop until complete

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import {
     rankWith,
-    isStringControl,
     RankedTester,
     isBooleanControl
   } from '@jsonforms/core';
 
-import
-    PolarisInputControl
- from './controls/PolarisInputControl';
+
 import PolarisBooleanControl from './controls/PolarisBooleanControl';
 
 
 export const polarisRenderers: { tester: RankedTester, renderer: any}[] = [
- { tester: rankWith(4, isStringControl), renderer: PolarisInputControl },
  { tester: rankWith(4, isBooleanControl), renderer: PolarisBooleanControl }
 ];
