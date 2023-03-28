deb9e1a1bdf4cc5fa9081bcfea259f8631e83e7d
Simon Barnes <simon@simonbarnes.me>
2023-03-28 09:01:27 +0000

    added new layout testers

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,11 @@ import {
 
 import PolarisBooleanControl from './controls/PolarisBooleanControl';
 import PolarisHorizontalLayout from './layouts/PolarisHorizontalLayout';
+import PolarisVerticalLayout from './layouts/PolarisVerticalLayout';
 
 
 export const polarisRenderers: { tester: RankedTester, renderer: any}[] = [
  { tester: rankWith(4, isBooleanControl), renderer: PolarisBooleanControl },
- { tester: rankWith(2, uiTypeIs('HorizontalLayout')), renderer: PolarisHorizontalLayout }
+ { tester: rankWith(2, uiTypeIs('HorizontalLayout')), renderer: PolarisHorizontalLayout },
+ { tester: rankWith(2, uiTypeIs('VerticalLayout')), renderer: PolarisVerticalLayout }
 ];
